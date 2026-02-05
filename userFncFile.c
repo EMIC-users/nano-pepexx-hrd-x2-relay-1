485698e31e34558ef80fa4f003f8752787d2488d
EMIC Compiler <emic-compiler@emic.io>
2026-02-04 22:28:38 -0300

    Code generation: 2026-02-05 01:28:38 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hrd-x2-relay-1
    Project: pepexx
    User: nanoloco2

---
+++ userFncFile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "inc/userFncFile.h"
+#include "inc/systemTimer.h"
+#include "inc/led_LED1.h"
+#include "inc/led_LED2.h"
+#include "inc/led_LED3.h"
+#include "inc/relay_Relay1.h"
+#include "inc/relay_Relay2.h"
+#include "inc/timer_api1.h"
+#include "inc/EMICBus.h"
+
+void onReset()
+{
+}
+
+
+
